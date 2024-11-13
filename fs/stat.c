0d3fe0a307acbbd19f1919cc7f51ac8a46f5b6dc
averagePedestrian <nehalchoudhury.16@gmail.com>
2024-11-13 09:17:33 +0000

    Add KSU hooks for stat.c

--- fs/stat.c
+++ fs/stat.c
@@ -173,9 +173,9 @@ int vfs_statx(int dfd, const char __user *filename, int flags,
 	struct path path;
 	int error = -EINVAL;
 	unsigned int lookup_flags = LOOKUP_FOLLOW | LOOKUP_AUTOMOUNT;
-#ifdef CONFIG_KSU
+   #ifdef CONFIG_KSU
 	ksu_handle_stat(&dfd, &filename, &flags);
-#endif
+   #endif
 	if ((flags & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |
 		       AT_EMPTY_PATH | KSTAT_QUERY_FLAGS)) != 0)
 		return -EINVAL;
