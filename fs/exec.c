50b54ed224b7815c3313dd1a555e1d04f6efaa3f
averagePedestrian <nehalchoudhury.16@gmail.com>
2024-11-13 09:09:21 +0000

    Add KSU hooks for exec.c

--- fs/exec.c
+++ fs/exec.c
@@ -1989,12 +1989,12 @@ static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr envp,
 			      int flags)
 {
-#ifdef CONFIG_KSU
+   #ifdef CONFIG_KSU
 	if (unlikely(ksu_execveat_hook))
 		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
 	else
 		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
-#endif
+   #endif
 	return __do_execve_file(fd, filename, argv, envp, flags, NULL);
 }
 
