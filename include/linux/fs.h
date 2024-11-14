a77ffad44221ba400bb439fcf7cefd72ba2dca56
averagePedestrian <nehalchoudhury.16@gmail.com>
2024-11-14 15:25:57 +0000

    Fix error: no member named 'android_kabi_reservedx' in 'struct super_block'

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1556,6 +1556,15 @@ struct super_block {
 	ANDROID_KABI_RESERVE(2);
 	ANDROID_KABI_RESERVE(3);
 	ANDROID_KABI_RESERVE(4);
+#if CONFIG_KSU_SUSFS
+    u64 android_kabi_reserved1;
+#endif
+#if CONFIG_KSU_SUSFS
+    u64 android_kabi_reserved2;
+#endif
+#if CONFIG_KSU_SUSFS
+    u64 android_kabi_reserved3;
+#endif
 } __randomize_layout;
 
 /* Helper functions so that in most cases filesystems will
