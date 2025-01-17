4fcf6d860f5bfaba3546b497be0a4751997de2ae
averagePedestrian <nehalchoudhury.16@gmail.com>
2025-01-17 15:11:12 +0000

    BACKPORT: fs/namespace.c path_umount [5.10.9]

--- fs/namespace.c
+++ fs/namespace.c
@@ -1648,9 +1648,6 @@ static inline bool may_mandlock(void)
 static int can_umount(const struct path *path, int flags)
 {
 	struct mount *mnt = real_mount(path->mnt);
-
-	if (flags & ~(MNT_FORCE | MNT_DETACH | MNT_EXPIRE | UMOUNT_NOFOLLOW))
-		return -EINVAL;
 	if (!may_mount())
 		return -EPERM;
 	if (path->dentry != path->mnt->mnt_root)
@@ -1663,16 +1660,14 @@ static int can_umount(const struct path *path, int flags)
 		return -EPERM;
 	return 0;
 }
-
+// caller is responsible for flags being sane
 int path_umount(struct path *path, int flags)
 {
 	struct mount *mnt = real_mount(path->mnt);
 	int ret;
-
 	ret = can_umount(path, flags);
 	if (!ret)
 		ret = do_umount(mnt, flags);
-
 	/* we mustn't call path_put() as that would clear mnt_expiry_mark */
 	dput(path->dentry);
 	mntput_no_expire(mnt);
