8cf9a384663bf1792895eb3eb5cd96973f9e5772
Nehal Choudhury <nehalchoudhury.16@gmail.com>
2025-04-07 13:58:53 +0530

    fs/proc/task_mmu: Limit scope of dentry in show_map_vma

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -374,14 +374,13 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma)
 	unsigned long start, end;
 	dev_t dev = 0;
 	const char *name = NULL;
-	struct dentry *dentry;
 
 	if (file) {
 		struct inode *inode = file_inode(vma->vm_file);
 		dev = inode->i_sb->s_dev;
 		ino = inode->i_ino;
 		pgoff = ((loff_t)vma->vm_pgoff) << PAGE_SHIFT;
-		dentry = file->f_path.dentry;
+		struct dentry *dentry = file->f_path.dentry;
 	if (dentry) {
 		const char *path = (const char *)dentry->d_name.name; 
 		if (strstr(path, "lineage") ||
