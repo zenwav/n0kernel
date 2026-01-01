dbb57fcc3cf26de6c56baeffb2d1d5580221f9a6
zenwav <nehalchoudhury.16@gmail.com>
2026-01-01 15:46:51 +0000

    Revert "treewide: import susfs4ksu v1.5.5 changes"
    This reverts commit bc5a2ecd28db65423212fc6bc565fed7b24efe73.

--- kernel/sys.c
+++ kernel/sys.c
@@ -1240,18 +1240,13 @@ static int override_release(char __user *release, size_t len)
 	return ret;
 }
 
-#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
-extern void susfs_spoof_uname(struct new_utsname* tmp);
-#endif
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
-#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
-	susfs_spoof_uname(&tmp);
-#endif
+
 	if (current_uid().val == 0 &&
 		(!strncmp(current->comm, "bpfloader", 9) ||
 		!strncmp(current->comm, "netbpfload", 10) ||
@@ -1260,6 +1255,7 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 		pr_info("fake uname: %s/%d release=%s\n",
 			 current->comm, current->pid, tmp.release);
 	}
+
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
