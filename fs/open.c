4f6acec2ab2f6bc2905ff0623801f851f92a9555
zenwav <nehalchoudhury.16@gmail.com>
2026-05-21 13:51:44 +0000

    Fix error: implicit declaration of function 'SUSFS_IS_INODE_OPEN_REDIRECT_WITHOUT_UID_CHECK'

--- fs/open.c
+++ fs/open.c
@@ -31,6 +31,9 @@
 #include <linux/ima.h>
 #include <linux/dnotify.h>
 #include <linux/compat.h>
+#if defined(CONFIG_KSU_SUSFS_SUS_MOUNT) || defined(CONFIG_KSU_SUSFS_OPEN_REDIRECT)
+#include <linux/susfs_def.h>
+#endif // #if defined(CONFIG_KSU_SUSFS_SUS_MOUNT) || defined(CONFIG_KSU_SUSFS_OPEN_REDIRECT)
 
 #include "internal.h"
 
